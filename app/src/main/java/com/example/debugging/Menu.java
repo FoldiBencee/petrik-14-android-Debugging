b98a46980a7d17416977fd165f5b769ea045f12d
foldibence <foldib555@gmail.com>
2020-01-09 16:32:38 +0100

    debugging orai

--- app/src/main/java/com/example/debugging/Menu.java
+++ app/src/main/java/com/example/debugging/Menu.java
@@ -39,9 +39,10 @@ public class Menu extends AppCompatActivity implements View.OnClickListener{
     {
         buttonExit = findViewById(R.id.buttonExit);
         buttonInformation = findViewById(R.id.buttonInformation);
-        buttonNameChange = findViewById(R.id.buttonBack);
+        buttonNameChange = findViewById(R.id.buttonNameChange);
         buttonNext = findViewById(R.id.buttonNext);
         textViewName = findViewById(R.id.textViewName);
+
 
         SharedPreferences sharedPreferences = getSharedPreferences("Adatok", Context.MODE_PRIVATE);
         seged = sharedPreferences.getString("Nev","Nincs elmentve a neved!");
@@ -86,6 +87,7 @@ public class Menu extends AppCompatActivity implements View.OnClickListener{
                 Toast.makeText(this, "A neved:" + seged, Toast.LENGTH_SHORT).show();
                 break;
             case R.id.buttonExit:
+
                 break;
             case R.id.buttonNext:
                 Intent next = new Intent(Menu.this,ThirdActivity.class);
